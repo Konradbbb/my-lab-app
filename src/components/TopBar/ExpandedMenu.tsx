a76e21d51fdfdce9a6d465bd3fccaa1c4bb9b653
konrad <konradbaran30@gmail.com>
2021-04-20 00:20:06 +0200

    small fixes in left menu, added dropdown menu

--- src/components/TopBar/ExpandedMenu.tsx
+++ src/components/TopBar/ExpandedMenu.tsx
@@ -3,11 +3,46 @@ import styled from 'styled-components';
  
 
 const ExpandedWrapperMenu = styled.div`
-    // border: 1px solid blue;
+    position: absolute;
+    width: 100%;
+    height: 800%;
+    margin-left: -0.47%;
+    background: white;
+    top: 100%;
+    border: 1px solid grey;
+    border-radius: 2px;
+`;
+
+const ExtendedMenuInput = styled.input`
+    width: 90%;
+    margin: 5%;
+    margin-left: 3%;
+
+    &:hover {
+        outline: none;
+    }
+    &:focus {
+        outline: none;
+    }
+`;
+
+const ExtendedMenuList = styled.li`
+    
 `;
 
 export const ExpandedMenu: FC = () => {
     return (
-        <ExpandedWrapperMenu/>
+        <ExpandedWrapperMenu>
+            <ul>
+                <ExtendedMenuInput placeholder="Filter..."/>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+            </ul>
+        </ExpandedWrapperMenu>
     );
 };
